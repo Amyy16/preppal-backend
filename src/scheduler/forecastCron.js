7e93f8adc4a58d5ce7744b1d6d704bcc892327b4
Amarachi <105382386+Amyy16@users.noreply.github.com>
2026-03-20 10:58:42 +0100

    Change daily forecast generation time to 11:05 AM
    Updated cron schedule to run daily at 11:05 AM instead of 6:00 AM.

--- src/scheduler/forecastCron.js
+++ src/scheduler/forecastCron.js
@@ -6,7 +6,7 @@ const BusinessRepository = require("../db/business.db");
 
 // Schedule: every day at 6 AM
 cron.schedule(
-  "0 6 * * *",
+  "5 11 * * *",
   async () => {
     try {
       console.log("Running daily forecast generation at 6 AM");
